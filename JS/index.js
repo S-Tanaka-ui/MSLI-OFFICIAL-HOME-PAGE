a8ee86f92cd19df928cae566d53516bad6e75e2d
S-Tanaka-ui <sona252anos@gmail.com>
2026-08-17 10:27:37 +0900

    スライドショーの機能を修正

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,7 @@ const newsimgList = [
     "img/newsimage1.jpeg",
     "img/newsimage2.jpeg",
     "img/newsimage3.jpeg",
+    "img/newsimage4.jpeg",
 ]
 
 for(var i = 0; i < newsimgList.length; i++){
@@ -9,3 +10,26 @@ for(var i = 0; i < newsimgList.length; i++){
     slide.innerHTML = "<img src='" + newsimgList[i] + "'>";
     document.querySelector(".slider_inner ul").appendChild(slide);
 }
+
+const buttons = document.querySelectorAll(".news_slide");
+const slider = document.querySelector(".slider_inner ul");
+let position = 0;
+const move = 260;
+
+//＜
+buttons[0].addEventListener("click", function(){
+    position += move;
+    if(position > 0){
+        position = 0;
+    }
+    slider.style.transform = `translateX(${position}px)`;
+});
+
+//＞
+buttons[1].addEventListener("click", function(){
+    position -= move;
+    if(position < -520){
+        position = -520;
+    }
+    slider.style.transform = `translateX(${position}px)`;
+});
